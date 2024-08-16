c653cfdda0d9339c5f7c35dd87b27a9b5751cc93
AnB School 0021 <anbschool0021@AnBs-21-Mac-mini.local>
2024-08-16 09:53:23 +0700

    init folder and file structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Posts from './components/Posts'
+
+const App = () => {
+  return (
+    <div>
+        <Posts />
+      {/* Add other routes here */}
+    </div>
+  )
+}
+
+export default App
